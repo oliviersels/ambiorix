04dcfa1d34cab4c30c0a261ba3d4a0507c25b204
Olivier Sels <olivier.sels@gmail.com>
2008-11-24 12:19:26 +0000

    JUnit voorbeeld

---
+++ ambiorix/src/ambiorix/tests/VoorbeeldTest.java
@@ -0,0 +1,20 @@
+package ambiorix.tests;
+
+// Javadoc is te vinden op http://junit.org/junit/javadoc/4.5/
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VoorbeeldTest {
+
+	@Test
+	public void testDoeIets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVraagIets() {
+		fail("Not yet implemented");
+	}
+
+}
